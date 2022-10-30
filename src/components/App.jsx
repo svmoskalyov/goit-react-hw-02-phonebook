1338d8f1efe5aef5dd2d3db3dadf6bbbe9ccbe92
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-30 10:59:01 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import Container from '../components/Container';
-import ContactForm from '../components/ContactForm';
-import ContactList from '../components/ContactList';
-import Filter from '../components/Filter';
+import Container from './Container';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import './App.module.css';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
+    name: '',
+    number: '',
   };
+  
 
   render() {
+    // console.log(this.state);
+
     return (
       <Container>
         <h1>Phonebook</h1>
