0c4f2cccdd22c86b7855b78e6a3143375f541d9a
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-30 13:14:36 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,23 @@
-// import styles from './Filter.module.css';
+import styles from './Filter.module.css';
 
-const Filter = () => {};
+const Filter = ({ value, onChange }) => {
+  return (
+    <>
+      <label className={styles.filter}>
+        Find contacts by name
+        <input
+          className={styles.input}
+          type="text"
+          name="filter"
+          value={value}
+          onChange={onChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+    </>
+  );
+};
 
 export default Filter;
